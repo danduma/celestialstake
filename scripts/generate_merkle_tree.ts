54274b19fd97e1b9d54307834d9ab8a717a02736
Daniel Duma <danielduma@gmail.com>
2022-07-20 20:39:01 +0200

    initial

---
+++ scripts/generate_merkle_tree.ts
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+
+const path=require("path");
+
+import { generateMerkleTree, PieceInfo } from '../test/nftStake/test_helpers.js';
+
+
+let rawdata = fs.readFileSync('attributes.json');
+let attributes = JSON.parse(rawdata);
+
+let keys =[...Object.keys(attributes) ];
+let piece_list = [];
+
+keys.forEach(element => {
+  
+  let piece = <PieceInfo>attributes[element];
+  // console.log(piece);
+  piece_list.push(piece);
+});
+
+// console.log(piece_list);
+
+let tree = generateMerkleTree(piece_list, null);
+// console.log(tree);
+
+tree.then( result => {
+  let data = JSON.stringify(result);
+  console.log(data);
+  fs.writeFileSync('merkle_proofs.json', data);
+})
+
