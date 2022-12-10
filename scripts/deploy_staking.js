e2cbab10854b0ce7e92424159abac1ef54c53f4c
Daniel Duma <danielduma@gmail.com>
2022-12-10 23:46:09 +0000

    update

--- scripts/deploy_staking.js
+++ scripts/deploy_staking.js
@@ -3,7 +3,7 @@ async function main() {
 
    const nft_address = "0xb5C48B932934B299Cf837D18753d1990a9B7722b";
    const erc20_address = "0x193AcA30704F0615B6444f565b42d7FE34f5A012";
-   const attributes_root = "0x15804ce24892e7aa75f7b1c4245ba76d643c2af808630b1e66605a82db855339";
+   const attributes_root = "0xd3a897bb85dabfe225e8d23c64bb92c9d4998365a3e37795fd85201dadf6755e";
    const admin = "0x57f5Ab4B82726a153396430dd45f3d1667DC335B";
    
    // Start deployment, returning a promise that resolves to a contract object
